e2cf6697a2e006151790d87f27edf08a0c9a190a
franciscojarmison <phantomrising@protonmail.com>
2025-06-04 10:52:28 -0300

    feat: add project structure initial

---
+++ src/main/java/com/jarmison/incassdev/IncassdevApplication.java
@@ -0,0 +1,13 @@
+package com.jarmison.incassdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncassdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncassdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarmison/incassdev/IncassdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarmison.incassdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncassdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
